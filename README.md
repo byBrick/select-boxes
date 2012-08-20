d7e1a9f920dc7c5878535c6841c58209bed354d4
Håkan Edling <hakan@tidyui.com>
2012-08-20 16:48:16 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,17 @@
+Select Boxes
+============
+Small jQuery/CSS snippet for creating responsive custom select boxes. The
+rendered html is fairly unstyled. Fork it, tweek it and style it the way
+you want.
+
+Html should be of the following form for the script to recognize it.
+
+	<div class="select">
+	  <select id="myId">
+	    <option>Option 1</option>
+	    <option>Option 2</option>
+	    ...
+	  </select>
+	</div>
+	
+Happy hacking!!!
---
+++ select-boxes.js
@@ -0,0 +1,58 @@
+/**
+ * Custom responsive select boxes.
+ *
+ * Copyright 2012 HŒkan Edling @tidyui.
+ */
+$(document).ready(function () {
+	/**
+	 * Find all custom select boxes and create the custom html.
+	 */
+	$.each($('.select select'), function(i, e) {
+		// Get all of the option elements.
+		var options = $(e).children('option') ;
+		
+		// Get the currently selected option.
+		var selected = $(e).children('option:selected').text() ;
+		// If nothing was selected, get the first option.
+		if (selected == null)
+			selected = $(children.get(0)).text() ;
+					
+		// Lets build some html	
+		var html =
+			'<div class="select-btn"></div>' +
+			'<div class="select-val">' + selected  + '</div>' +
+			'<ul class="select-opt" data-id="' + $(e).attr('id') + '">' ;
+		
+		for (var n = 0; n < options.length; n++) {
+			html += '<li>' + $(options[n]).text() + '</li>' ;
+		}
+		html += '</ul>' ;
+		
+		// Append the html to the parent container.
+		$(e).parent().append(html);
+	});
+
+	/**
+	 * Add the click event to the custom select box.
+	 */
+	$('.select-btn, .select-val').click(function() {
+		$(this).siblings('.select-opt').fadeToggle('fast');
+	});
+	
+	/**
+	 * Add the click event to the custom select option.
+	 */
+	$('.select-opt li').click(function() {
+		// Get the matching select box and the selected index.
+		var select = $('#' + $(this).parent().attr('data-id')) ;
+		var index = $(this).parent().find('li').index(this) ;
+
+		// Select the new option in the "real" select list.
+		select.children('option').removeAttr('selected');
+		$(select.children('option').get(index)).attr('selected', 'selected') ;
+		
+		// Update & close the custom select list.
+		$(this).parent().siblings('.select-val').text($(this).text()) ;
+		$(this).parent().fadeToggle('fast');
+	});
+});
